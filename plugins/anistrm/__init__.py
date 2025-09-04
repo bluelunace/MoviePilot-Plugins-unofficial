5edf86d825254ee6e73fec6d96aa01468557d747
bluelunace <bluelunace@qq.com>
2025-09-05 00:32:45 +0800

    Update __init__.py 2.6.6
    rss中link有错误，本来应该是.mp4现在却是?d=mp4，需要修正
    import re
    
    rss_info['link'] = unquote(
        re.sub(r'\?d=mp4$', '.mp4', link.replace("resources.ani.rip", "ani.v300.eu.org"))
    )

--- plugins/anistrm/__init__.py
+++ plugins/anistrm/__init__.py
@@ -17,6 +17,7 @@ from bs4 import BeautifulSoup
 import requests
 from app.helper.browser import PlaywrightHelper
 from urllib.parse import unquote
+import re
 
 def retry(ExceptionToCheck: Any,
           tries: int = 3, delay: int = 3, backoff: int = 1, logger: Any = None, ret: Any = None):
@@ -61,7 +62,7 @@ class ANiStrm(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/honue/MoviePilot-Plugins/main/icons/anistrm.png"
     # 插件版本
-    plugin_version = "2.6.5"
+    plugin_version = "2.6.6"
     # 插件作者
     plugin_author = "honue,bluelunace"
     # 作者主页
@@ -232,7 +233,8 @@ class ANiStrm(_PluginBase):
             # 链接
             link = DomUtils.tag_value(item, "link", default="")
             rss_info['title'] = title
-            rss_info['link'] = unquote(link.replace("resources.ani.rip", "ani.v300.eu.org"))
+        #    rss_info['link'] = unquote(link.replace("resources.ani.rip", "ani.v300.eu.org"))
+            rss_info['link'] = unquote(re.sub(r'\?d=mp4', '.mp4', link.replace("resources.ani.rip", "ani.v300.eu.org")))
             ret_array.append(rss_info)
         return ret_array
 
